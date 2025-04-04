efc9c4c57867ac1cc7bea32a99b52c19850f0d5b
Natt - <nordonez376@gmail.com>
2025-04-04 14:41:46 -0500

    practica

--- ACTIVIDAD CASA 1 CICLOS/script.js
+++ ACTIVIDAD CASA 1 CICLOS/script.js
@@ -1,19 +1,18 @@
 //genere un numero aleatorio entre el 1 y el 10
 
-let intento
+let intento = parseInt(prompt("Adivina el numero entre el 1 y el 10: "))
 
 let numeroRandom = Math.floor(Math.random() * 10) + 1 //numero randomm del 10 al 1
 
 
 // != --- Signfica "Distinto de" // == ---- Compara solo el valor, sin importar el tipo de dato.
 while (intento !== numeroRandom) { 
-    intento = parseInt(prompt("Adivina el numero entre el 1 y el 10: "))
 
     if ( intento > numeroRandom) {
-        console.log("Muy mayor, intenta de nuevo")
-    } else  if (intento < numeroRandom) {
-        console.log("Muy menor, intenta de nuevo")
+    intento = parseInt(prompt("Muy mayor, intenta de nuevo"))
     } else {
-        console.log("Lo lograste, adivinaste el numero")
-    }
+    intento =  parseInt(prompt("Muy menor, intenta de nuevo"))
+    } 
 } 
+
+alert("Lo lograste, adivinaste el número " + numeroRandom) //te manda el mensaje tipo prompt sin escribir nada
