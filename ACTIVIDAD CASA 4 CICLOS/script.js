d244acc6908b9b9b7176a1290e0572c824e7df2d
Natt - <nordonez376@gmail.com>
2025-04-04 15:48:25 -0500

    aaa

--- ACTIVIDAD CASA 4 CICLOS/script.js
+++ ACTIVIDAD CASA 4 CICLOS/script.js
@@ -2,16 +2,17 @@
 
 let opcion = ""
 
-while (opcion !== "3") { // con tal de que sea diferente a tres el menu permanecera
+
+do {
     opcion = prompt("1. Saludooo  " + " 2. Mensaje cute  " + " 3. Salidaa  ")
 
     if (opcion === "1") {
-        console.log("Que tengas un bonito día :)")
+        alert("Que tengas un bonito día :)")
     } else if (opcion === "2") {
-        console.log("⋅˚₊‧ ୨୧ ‧₊˚ ⋅uwu⋅˚₊‧ ୨୧ ‧₊˚ ⋅")
+        alert("⋅˚₊‧ ୨୧ ‧₊˚ ⋅uwu⋅˚₊‧ ୨୧ ‧₊˚ ⋅")
     } else if (opcion === "3") {
-        console.log("Saliendo del menú... te extrañaremos!")
+        alert("Saliendo del menú... te extrañaremos!")
     } else {
-        console.log("Opcion no válida, intenta de nuevo")
+        alert("Opcion no válida, intenta de nuevo")
     }
-}
+} while (opcion !== "3")  // con tal de que sea diferente a tres el menu permanecera
